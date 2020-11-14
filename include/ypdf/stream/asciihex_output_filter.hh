5c6bee53ccf20d0b23f189e3d5b1d84773e1d915
Liviu Nicoara <lnicoara at thinkoid dot org>
2020-11-13 19:16:51 -0500

    Simplified expression.

--- include/ypdf/stream/asciihex_output_filter.hh
+++ include/ypdf/stream/asciihex_output_filter.hh
@@ -18,9 +18,14 @@ struct asciihex_output_filter_t : public boost::iostreams::output_filter
     {
         static const char *s = "0123456789ABCDEF";
 
-        return !eof_ &&
-               boost::iostreams::put(dst, s[((unsigned)c & 0xF0) >> 4]) &&
-               boost::iostreams::put(dst, s[(unsigned)c & 0x0F]);
+        if (eof_)
+            return false;
+
+        const auto u = static_cast< unsigned char >(c);
+
+        return
+            ::boost::iostreams::put(dst, s[(u & 0xF0) >> 4]) &&
+            ::boost::iostreams::put(dst, s[ u & 0x0F]);
     }
 
     template< typename Sink > void close(Sink &dst)
